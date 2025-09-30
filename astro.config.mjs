63b96fb932d4ba96d374506c7f5be38c99911fbb
paulo-curvello <prgbarata@gmail.com>
2025-09-30 17:36:32 -0300

    Adição de contatos

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
+import icon from 'astro-icon';
 
 export default defineConfig({
   site: "https://dev.paulocurvello.com",
   base: "/",
   trailingSlash: "ignore",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), icon()],
   redirects: {
     '/': '/en/blog',
   },
